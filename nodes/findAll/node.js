b46fdea4aaf827a7a9a5d3790c8a02826df22c80
Rob Halff <rob.halff@gmail.com>
2014-03-04 17:35:10 +0100

    update

--- nodes/findAll/node.js
+++ nodes/findAll/node.js
@@ -1,9 +1,1 @@
-on.input[':start'] = function() {
-  input.collection.find(function(err, data) {
-    if(err) {
-      output({ error: err });
-    } else {
-      output({ documents: data });
-    }
-  });
-}
+output = [input.collection, 'find']
