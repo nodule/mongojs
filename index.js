f642721a22b4d133f7f8004181ccdd809b8f6f05
Rob Halff <rob.halff@gmail.com>
2014-12-28 22:28:15 +0100

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  close: require('./close'),
+  collection: require('./collection'),
+  db: require('./db'),
+  find: require('./find'),
+  findAll: require('./findAll'),
+  insert: require('./insert'),
+  remove: require('./remove'),
+  update: require('./update')
+}
